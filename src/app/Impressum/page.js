0ff6422670e850142518f4861d80104bc02d9bfe
Andrej Hristovski <andrejhristovski@yahoo.com>
2023-07-16 13:29:41 +0200

    Refactor with tailwind

--- src/app/Impressum/page.js
+++ src/app/Impressum/page.js
@@ -2,96 +2,179 @@ import React from "react";
 
 function ImpressumPage() {
   return (
-    <div>
-      <h1>Impressum</h1>
-      <p>
-        Fahrschulring GmbH<br />
-        Hegelstr. 48<br />
-        70174 Stuttgart
-      </p>
-      <p>
-        Vertreten durch:<br />
-        Frank Eibl
-      </p>
-      <p>
-        Kontakt:<br />
-        Telefon: 0711 - 295928<br />
-        Telefax: 0711 - 295928<br />
-        E-Mail: info(at)fahrschulring(dot)de
-      </p>
-      <p>
-        Registereintrag:<br />
-        Eintragung im Handelsregister.<br />
-        Registergericht: Stuttgart<br />
-        Registernummer: 14308
-      </p>
-      <p>
-        Umsatzsteuer-ID:<br />
-        Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:<br />
-        9906306056
-      </p>
-      <p>
-        Aufsichtsbehörde:<br />
-        Führerscheinstelle Stuttgart
-      </p>
-      <h2>Datenschutz</h2>
-      <p>
-        Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich.
-        Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder E-Mail-Adressen)
-        erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis.
-        Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.
-        Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann.
-        Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
-        Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen.
-        Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
-      </p>
-      <h2>Streitschlichtung</h2>
-      <p>
-        Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr</a>
-        <br />
-        Unsere E-Mail-Adresse finden Sie oben im Impressum.
-        Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.
-      </p>
-      <h2>Haftung für Inhalte</h2>
-      <p>
-        Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich.
-        Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
-        Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
-        Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich.
-        Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
-      </p>
-      <h2>Haftung für Links</h2>
-      <p>
-        Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben.
-        Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen.
-        Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich.
-        Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.
-        Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
-        Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar.
-        Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
-      </p>
-      <h2>Urheberrecht</h2>
-      <p>
-        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht.
-        Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
-        Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
-        Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet.
-        Insbesondere werden Inhalte Dritter als solche gekennzeichnet.
-        Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis.
-        Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
-      </p>
-      <p>
-        Quelle: <a href="https://www.e-recht24.de/impressum-generator.html" target="_blank" rel="noopener noreferrer">https://www.e-recht24.de/impressum-generator.html</a>
-      </p>
-      <h2>Technischer Ansprechpartner, Gestaltung und Layout</h2>
-      <p>
-        VitaByte Webdesign & Content Management<br />
-        Internet: <a href="https://www.redk.de" target="_blank" rel="noopener noreferrer">www.redk.de</a><br />
-        E-Mail: info@vitabyte.com
-      </p>
-      <p>
-        Quellenangaben: eRecht24 Disclaimer
-      </p>
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="px-4 sm:px-0">
+        <h1 className="text-2xl font-semibold leading-7 text-gray-900">
+          Impressum
+        </h1>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <div className="space-y-6">
+          <p className="text-base text-gray-900">
+            Fahrschulring GmbH
+            <br />
+            Hegelstr. 48
+            <br />
+            70174 Stuttgart
+          </p>
+          <p className="text-base text-gray-900">
+            Vertreten durch:
+            <br />
+            Frank Eibl
+          </p>
+          <p className="text-base text-gray-900">
+            Kontakt:
+            <br />
+            Telefon: 0711 - 295928
+            <br />
+            Telefax: 0711 - 295928
+            <br />
+            E-Mail: info(at)fahrschulring(dot)de
+          </p>
+          <p className="text-base text-gray-900">
+            Registereintrag:
+            <br />
+            Eintragung im Handelsregister.
+            <br />
+            Registergericht: Stuttgart
+            <br />
+            Registernummer: 14308
+          </p>
+          <p className="text-base text-gray-900">
+            Umsatzsteuer-ID:
+            <br />
+            Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:
+            <br />
+            9906306056
+          </p>
+          <p className="text-base text-gray-900">
+            Aufsichtsbehörde:
+            <br />
+            Führerscheinstelle Stuttgart
+          </p>
+          <h2 className="text-lg font-semibold leading-7 text-gray-900">
+            Datenschutz
+          </h2>
+          <p className="text-base text-gray-900">
+            Die Nutzung unserer Webseite ist in der Regel ohne Angabe
+            personenbezogener Daten möglich. Soweit auf unseren Seiten
+            personenbezogene Daten (beispielsweise Name, Anschrift oder
+            E-Mail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets
+            auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche
+            Zustimmung nicht an Dritte weitergegeben. Wir weisen darauf hin,
+            dass die Datenübertragung im Internet (z.B. bei der Kommunikation
+            per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz
+            der Daten vor dem Zugriff durch Dritte ist nicht möglich. Der
+            Nutzung von im Rahmen der Impressumspflicht veröffentlichten
+            Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich
+            angeforderter Werbung und Informationsmaterialien wird hiermit
+            ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich
+            ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung
+            von Werbeinformationen, etwa durch Spam-Mails, vor.
+          </p>
+          <h2 className="text-lg font-semibold leading-7 text-gray-900">
+            Streitschlichtung
+          </h2>
+          <p className="text-base text-gray-900">
+            Die Europäische Kommission stellt eine Plattform zur
+            Online-Streitbeilegung (OS) bereit:{" "}
+            <a
+              href="https://ec.europa.eu/consumers/odr"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600"
+            >
+              https://ec.europa.eu/consumers/odr
+            </a>
+            <br />
+            Unsere E-Mail-Adresse finden Sie oben im Impressum. Wir sind nicht
+            bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+            Verbraucherschlichtungsstelle teilzunehmen.
+          </p>
+          <h2 className="text-lg font-semibold leading-7 text-gray-900">
+            Haftung für Inhalte
+          </h2>
+          <p className="text-base text-gray-900">
+            Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte
+            auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach
+            §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
+            verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
+            überwachen oder nach Umständen zu forschen, die auf eine
+            rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung
+            oder Sperrung der Nutzung von Informationen nach den allgemeinen
+            Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist
+            jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten
+            Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden
+            Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+          </p>
+          <h2 className="text-lg font-semibold leading-7 text-gray-900">
+            Haftung für Links
+          </h2>
+          <p className="text-base text-gray-900">
+            Unser Angebot enthält Links zu externen Websites Dritter, auf deren
+            Inhalte wir keinen Einfluss haben. Deshalb können wir für diese
+            fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+            verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber
+            der Seiten verantwortlich. Die verlinkten Seiten wurden zum
+            Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.
+            Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht
+            erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten
+            Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung
+            nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir
+            derartige Links umgehend entfernen.
+          </p>
+          <h2 className="text-lg font-semibold leading-7 text-gray-900">
+            Urheberrecht
+          </h2>
+          <p className="text-base text-gray-900">
+            Die durch die Seitenbetreiber erstellten Inhalte und Werke auf
+            diesen Seiten unterliegen dem deutschen Urheberrecht. Die
+            Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+            Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der
+            schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+            Downloads und Kopien dieser Seite sind nur für den privaten, nicht
+            kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser
+            Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte
+            Dritter beachtet. Insbesondere werden Inhalte Dritter als solche
+            gekennzeichnet. Sollten Sie trotzdem auf eine
+            Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+            entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
+            werden wir derartige Inhalte umgehend entfernen.
+          </p>
+          <p className="text-base text-gray-900">
+            Quelle:{" "}
+            <a
+              href="https://www.e-recht24.de/impressum-generator.html"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600"
+            >
+              https://www.e-recht24.de/impressum-generator.html
+            </a>
+          </p>
+          <h2 className="text-lg font-semibold leading-7 text-gray-900">
+            Technischer Ansprechpartner, Gestaltung und Layout
+          </h2>
+          <p className="text-base text-gray-900">
+            VitaByte Webdesign &amp; Content Management
+            <br />
+            Internet:{" "}
+            <a
+              href="https://www.redk.de"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600"
+            >
+              www.redk.de
+            </a>
+            <br />
+            E-Mail: info@vitabyte.com
+          </p>
+          <p className="text-base text-gray-900">
+            Quellenangaben: eRecht24 Disclaimer
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/data_protection/page.js
+++ src/app/data_protection/page.js
@@ -2,14 +2,23 @@ import React from "react";
 
 function DataProtectionPage() {
   return (
-        <div>
-            <h1>Datenschutz</h1>
-            <p>Der Schutz Ihrer Privatsphäre und Ihrer persönlichen Daten bei der Nutzung unserer Websites sind uns wichtig. 
-            Wir halten uns strikt an die Regeln der Datenschutzgesetze.
-            Im Folgenden erläutern wir, welche Informationen wir während Ihres Besuchs auf unseren Websites erfassen und wie diese Informationen genutzt werden.
-            </p>
-        </div>
-    );
+    <div className="max-w-2xl mx-auto px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="px-4 sm:px-0">
+        <h1 className="text-2xl font-semibold leading-7 text-gray-900">
+          Datenschutz
+        </h1>
+      </div>
+      <div className="mt-6">
+        <p className="text-base text-gray-900">
+          Der Schutz Ihrer Privatsphäre und Ihrer persönlichen Daten bei der
+          Nutzung unserer Websites sind uns wichtig. Wir halten uns strikt an
+          die Regeln der Datenschutzgesetze. Im Folgenden erläutern wir, welche
+          Informationen wir während Ihres Besuchs auf unseren Websites erfassen
+          und wie diese Informationen genutzt werden.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default DataProtectionPage;
